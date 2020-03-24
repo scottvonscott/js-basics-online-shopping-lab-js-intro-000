0a9a29928f0f495246e08934381d5a369ba6cda4
Scott Keplinger <skeplin17@gmail.com>
2020-03-24 21:01:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,22 +18,19 @@ function addToCart(item) {
   return `${item} has been added to your cart.`
 }
 
-// function viewCart() {
-//   if (cart.length === 0) {
-//     return "Your shopping cart is empty."
-//   } else if (cart.length === 1){
-//       return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
-//   } else if (cart.length === 2) {
-//       return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
-//   } else {
-//       return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`
-//   }
-// }
+function viewCart() {
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1){
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  } else if (cart.length === 2) {
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  } else {
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`
+  }
+}
 
-} else {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`
-}
-}
+
 
 
 function total() {
