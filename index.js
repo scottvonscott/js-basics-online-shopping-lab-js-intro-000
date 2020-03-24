b84a9745c2e8c74e3884b84faab7ff5830342565
Scott Keplinger <skeplin17@gmail.com>
2020-03-24 20:13:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function addToCart(item) {
 
 function viewCart() {
   if (cart.length === 0) {
-    return "Your shopping cart is empty"
+    return "Your shopping cart is empty."
   } else if (cart.length === 1){
       return `In your cart, you have ${cart[0].itenName}.`
   } else if (cart.length === 2) {
